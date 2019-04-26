d71d61fd81c1e7f2f394e765b55345e2d696596d
juanlu-28a@hotmail.com <juanlu-28a@hotmail.com>
2019-04-26 13:22:33 +0200

    feat: create simple terraform module for simple ecs service

---
+++ README.md
@@ -0,0 +1,3 @@
+## ECS service
+
+This module is to create a ecs service. It creates task definition, service, lg target group, route 53 records and lb listener rules
---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "name" {
+  description = "Name of the service"
+}
+
+variable "container_definition_template" {
+  description = "Task container definition"
+}
+
+variable "container_definition_vars" {
+  description = "Container definition vars"
+  type        = "map"
+}
+
+variable "service_enable" {
+  description = "Active o deactivate service"
+}
+
+variable "cluster_id" {
+  description = "Id of the cluster"
+}
+
+variable "number_of_services" {
+  description = "Numer of services to deploy in the cluster"
+}
+
+variable "role_arn" {
+  description = "Id of the role to deploy the service"
+}
+
+variable "deployment_minimum_healthy_percent" {
+  description = "Minimum healthy percent of the service in the cluster"
+}
+
+variable depends_on {
+  default = []
+
+  type = "list"
+}
+
+variable "health_check_grace_period_seconds" {
+  description = "Health check grace period in seconds. It is the time that the app has to start."
+  default     = 300
+}
+
+variable "container_name" {}
+variable "container_port" {}
+variable "vpc_id" {}
+
+variable "health_check_path" {
+  default = "/health"
+}
+
+variable "route53_records_zone_id" {}
+variable "route53_records_name" {}
+variable "route53_alias_name" {}
+variable "route53_alias_zone_id" {}
+variable "lb_listener_rule_listener_arn" {}
